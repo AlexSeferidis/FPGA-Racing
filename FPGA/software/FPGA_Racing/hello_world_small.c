809268a6d0492b8444478822893254c0938a5405
Gurjan-Singh-Samra <gsamra474@gmail.com>
2024-03-20 15:02:42 +0000

    added code for fpga

--- FPGA/software/FPGA_Racing/hello_world_small.c
+++ FPGA/software/FPGA_Racing/hello_world_small.c
@@ -1,91 +1,164 @@
-/* 
- * "Small Hello World" example. 
- * 
- * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
- * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example 
- * designs. It requires a STDOUT  device in your system's hardware. 
- *
- * The purpose of this example is to demonstrate the smallest possible Hello 
- * World application, using the Nios II HAL library.  The memory footprint
- * of this hosted application is ~332 bytes by default using the standard 
- * reference design.  For a more fully featured Hello World application
- * example, see the example titled "Hello World".
- *
- * The memory footprint of this example has been reduced by making the
- * following changes to the normal "Hello World" example.
- * Check in the Nios II Software Developers Manual for a more complete 
- * description.
- * 
- * In the SW Application project (small_hello_world):
- *
- *  - In the C/C++ Build page
- * 
- *    - Set the Optimization Level to -Os
- * 
- * In System Library project (small_hello_world_syslib):
- *  - In the C/C++ Build page
- * 
- *    - Set the Optimization Level to -Os
- * 
- *    - Define the preprocessor option ALT_NO_INSTRUCTION_EMULATION 
- *      This removes software exception handling, which means that you cannot 
- *      run code compiled for Nios II cpu with a hardware multiplier on a core 
- *      without a the multiply unit. Check the Nios II Software Developers 
- *      Manual for more details.
- *
- *  - In the System Library page:
- *    - Set Periodic system timer and Timestamp timer to none
- *      This prevents the automatic inclusion of the timer driver.
- *
- *    - Set Max file descriptors to 4
- *      This reduces the size of the file handle pool.
- *
- *    - Check Main function does not exit
- *    - Uncheck Clean exit (flush buffers)
- *      This removes the unneeded call to exit when main returns, since it
- *      won't.
- *
- *    - Check Don't use C++
- *      This builds without the C++ support code.
- *
- *    - Check Small C library
- *      This uses a reduced functionality C library, which lacks  
- *      support for buffering, file IO, floating point and getch(), etc. 
- *      Check the Nios II Software Developers Manual for a complete list.
- *
- *    - Check Reduced device drivers
- *      This uses reduced functionality drivers if they're available. For the
- *      standard design this means you get polled UART and JTAG UART drivers,
- *      no support for the LCD driver and you lose the ability to program 
- *      CFI compliant flash devices.
- *
- *    - Check Access device drivers directly
- *      This bypasses the device file system to access device drivers directly.
- *      This eliminates the space required for the device file system services.
- *      It also provides a HAL version of libc services that access the drivers
- *      directly, further reducing space. Only a limited number of libc
- *      functions are available in this configuration.
- *
- *    - Use ALT versions of stdio routines:
- *
- *           Function                  Description
- *        ===============  =====================================
- *        alt_printf       Only supports %s, %x, and %c ( < 1 Kbyte)
- *        alt_putstr       Smaller overhead than puts with direct drivers
- *                         Note this function doesn't add a newline.
- *        alt_putchar      Smaller overhead than putchar with direct drivers
- *        alt_getchar      Smaller overhead than getchar with direct drivers
- *
- */
+#include "system.h"
+#include "altera_up_avalon_accelerometer_spi.h"
+#include "altera_avalon_timer_regs.h"
+#include "altera_avalon_timer.h"
+#include "altera_avalon_pio_regs.h"
+#include "sys/alt_irq.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "sys/alt_stdio.h"  // Include Altera HAL for UART functions
 
-#include "sys/alt_stdio.h"
+#define OFFSET -32
+#define PWM_PERIOD 16
 
-int main()
-{ 
-  alt_putstr("Hello from Nios II!\n");
+#define CHARLIM 256    // Maximum character length of what the user places in memory.  Increase to allow longer sequences
+#define QUITLETTER '~' // Letter to kill all processing
 
-  /* Event loop never exits. */
-  while (1);
+alt_8 pwm = 0;
+alt_u8 led;
+int level;
+int TAPS = 64;
 
-  return 0;
+void led_write(alt_u8 led_pattern) {
+    IOWR(LED_BASE, 0, led_pattern);
+}
+
+void convert_read(alt_32 acc_read, int * level, alt_u8 * led) {
+//    acc_read += OFFSET;
+    alt_u8 val = (acc_read >> 6) & 0x07;
+    * led = (8 >> val) | (8 << (8 - val));
+    * level = (acc_read >> 1) & 0x1f;
+}
+
+void sys_timer_isr() {
+    IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_BASE, 0);
+
+    if (pwm < abs(level)) {
+
+        if (level < 0) {
+            led_write(led << 1);
+        } else {
+            led_write(led >> 1);
+        }
+
+    } else {
+        led_write(led);
+    }
+
+    if (pwm > PWM_PERIOD) {
+        pwm = 0;
+    } else {
+        pwm++;
+    }
+
+}
+
+void timer_init(void * isr) {
+
+    IOWR_ALTERA_AVALON_TIMER_CONTROL(TIMER_BASE, 0x0003);
+    IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_BASE, 0);
+    IOWR_ALTERA_AVALON_TIMER_PERIODL(TIMER_BASE, 0x0900);
+    IOWR_ALTERA_AVALON_TIMER_PERIODH(TIMER_BASE, 0x0000);
+    alt_irq_register(TIMER_IRQ, 0, isr);
+    IOWR_ALTERA_AVALON_TIMER_CONTROL(TIMER_BASE, 0x0007);
+
+}
+
+alt_32 applyFilter(alt_32 new_reading, alt_32 filter[TAPS], alt_32 coeff[TAPS]){
+	alt_32 filtered_reading = 0;
+
+	for(int i = TAPS - 1; i > 0; i--){
+		filter[i] =  filter[i-1];
+	}
+
+	filter[0] = new_reading;
+
+	for(int i = 0; i < TAPS; i++){
+		filtered_reading += filter[i] * coeff[i];
+	}
+	return filtered_reading >> 6;
+}
+
+int main() {
+	printf("Hello, Sam!\n");
+	int i = 0;
+    alt_32 x_read [TAPS];
+    alt_32 y_read [TAPS];
+    alt_32 coeff[TAPS];
+    alt_32 val;
+    alt_32 direction=0;
+    alt_32 drive=0;
+    int switch_datain;
+
+    for(int i = 0; i < TAPS; i++){
+    	x_read[i] = 0;
+    	y_read[i] = 0;
+    	coeff[i] = 1;
+    }
+
+
+    alt_up_accelerometer_spi_dev * acc_dev;
+    acc_dev = alt_up_accelerometer_spi_open_dev("/dev/accelerometer_spi");
+    if (acc_dev == NULL) { // if return 1, check if the spi ip name is "accelerometer_spi"
+        return 1;
+    }
+
+    timer_init(sys_timer_isr);
+    while (1) {
+
+    	//Gets the data from the pb, recall that a 0 means the button is pressed
+    	switch_datain = ~IORD_ALTERA_AVALON_PIO_DATA(BUTTON_BASE);
+    	//Mask the bits so the leftmost LEDs are off (we only care about LED3-0)
+    	switch_datain &= (0b0000000011);
+
+    	if (i == TAPS){
+    		i = 0;
+    	}
+        alt_up_accelerometer_spi_read_x_axis(acc_dev, & val);
+//        alt_32 avg_x = applyFilter(val, x_read, coeff);
+        alt_32 avg_x = val;
+        alt_up_accelerometer_spi_read_y_axis(acc_dev, & val);
+        alt_32 avg_y = applyFilter(val, y_read, coeff);
+
+        //buttons
+        if(switch_datain == 0b01){
+        	alt_printf("1/0/");
+        }
+        if(switch_datain == 0b10){
+        	alt_printf("0/1/");
+        }
+        if(switch_datain == 0b11){
+        	alt_printf("1/1/");
+        }
+        if(switch_datain == 0b00){
+        	alt_printf("0/0/");
+        }
+
+        // direction
+        if(avg_x < -41){
+        	alt_printf("-1/");
+        }
+        else if(avg_x > 41){
+        	alt_printf("1/");
+        }
+        else{
+        	alt_printf("0/");
+        }
+
+        //steering
+        if(avg_y < -80){
+        	alt_printf("1\n");
+        }
+        else if(avg_y > 80){
+        	alt_printf("-1\n");
+        }
+        else{
+        	alt_printf("0\n");
+        }
+
+        i++;
+    }
+
+    return 0;
 }
--- FPGA/software/FPGA_Racing/obj/default/hello_world_small.d
+++ FPGA/software/FPGA_Racing/obj/default/hello_world_small.d
@@ -1,4 +1,53 @@
 obj/default/hello_world_small.o: hello_world_small.c \
+ ../FPGA_Racing_bsp/system.h ../FPGA_Racing_bsp/linker.h \
+ ../FPGA_Racing_bsp//drivers/inc/altera_up_avalon_accelerometer_spi.h \
+ ../FPGA_Racing_bsp//HAL/inc/alt_types.h \
+ ../FPGA_Racing_bsp//HAL/inc/sys/alt_dev.h \
+ ../FPGA_Racing_bsp//HAL/inc/sys/alt_llist.h \
+ ../FPGA_Racing_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../FPGA_Racing_bsp//drivers/inc/altera_up_avalon_accelerometer_spi_regs.h \
+ ../FPGA_Racing_bsp//HAL/inc/io.h ../FPGA_Racing_bsp//HAL/inc/alt_types.h \
+ ../FPGA_Racing_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../FPGA_Racing_bsp//drivers/inc/altera_avalon_timer.h \
+ ../FPGA_Racing_bsp//HAL/inc/sys/alt_warning.h \
+ ../FPGA_Racing_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../FPGA_Racing_bsp//HAL/inc/sys/alt_irq.h \
+ ../FPGA_Racing_bsp//HAL/inc/nios2.h \
+ ../FPGA_Racing_bsp//HAL/inc/priv/alt_legacy_irq.h \
  ../FPGA_Racing_bsp//HAL/inc/sys/alt_stdio.h
+
+../FPGA_Racing_bsp/system.h:
+
+../FPGA_Racing_bsp/linker.h:
+
+../FPGA_Racing_bsp//drivers/inc/altera_up_avalon_accelerometer_spi.h:
+
+../FPGA_Racing_bsp//HAL/inc/alt_types.h:
+
+../FPGA_Racing_bsp//HAL/inc/sys/alt_dev.h:
+
+../FPGA_Racing_bsp//HAL/inc/sys/alt_llist.h:
+
+../FPGA_Racing_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../FPGA_Racing_bsp//drivers/inc/altera_up_avalon_accelerometer_spi_regs.h:
+
+../FPGA_Racing_bsp//HAL/inc/io.h:
+
+../FPGA_Racing_bsp//HAL/inc/alt_types.h:
+
+../FPGA_Racing_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../FPGA_Racing_bsp//drivers/inc/altera_avalon_timer.h:
+
+../FPGA_Racing_bsp//HAL/inc/sys/alt_warning.h:
+
+../FPGA_Racing_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../FPGA_Racing_bsp//HAL/inc/sys/alt_irq.h:
+
+../FPGA_Racing_bsp//HAL/inc/nios2.h:
+
+../FPGA_Racing_bsp//HAL/inc/priv/alt_legacy_irq.h:
 
 ../FPGA_Racing_bsp//HAL/inc/sys/alt_stdio.h:
